3651d105a907e9269a5b2da29148fda003ad9445
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2023-10-30 19:50:52 +0200

    update

--- src/pages/EditBook.js
+++ src/pages/EditBook.js
@@ -16,7 +16,7 @@ const EditBook = () => {
 
   useEffect(() => {
     setLoading(true);
-    axios.get(`http://localhost:5555/books/${id}`)
+    axios.get(`https://bookstore-backend-5t2o.onrender.com/books/${id}`)
     .then((response) => {
         setAuthor(response.data.author);
         setPublishYear(response.data.publishYear)
